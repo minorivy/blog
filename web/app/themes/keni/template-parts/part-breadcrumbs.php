2b18b04b612cd6ccd3886aeb18576f9a47e886d1
kento <acd.thoughts+dev@gmail.com>
2019-09-01 15:47:30 +0900

    remove and ignore the parent theme

--- web/app/themes/keni/template-parts/part-breadcrumbs.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! ( is_front_page() && ! is_paged() ) ) : ?>
-<!--▼▼ パン屑リスト ▼▼-->
-<div class="keni-breadcrumb-list_wrap">
-	<div class="keni-breadcrumb-list_outer">
-<?php the_keni_breadcrumbs(); ?>
-	</div><!--keni-breadcrumb-list_outer-->
-</div><!--keni-breadcrumb-list_wrap-->
-<!--▲▲ パン屑リスト ▲▲-->
-<?php endif; ?>
