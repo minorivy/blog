06c80edde2d7565c4553192329ace8b53a7bafff
kento <acd.thoughts+dev@gmail.com>
2019-09-21 23:06:25 +0900

    fix link attr, fix style

--- web/blog/app/themes/aytc/core/posts/init.php
+++ web/blog/app/themes/aytc/core/posts/init.php
@@ -1,1 +1,20 @@
 <?php
+
+// Replace anchor link attrs in post content
+add_filter( 'the_content', function( $string ) {
+	return preg_replace_callback(
+		'/<a\b([^>]*)href=(?:\'|")(.*?)(?:\'|")([^>]*)>(.*?)<\/a>/',
+		function($matches) {
+			$href_link = $matches[2];
+			if (strpos($href_link, $_SERVER['HTTP_HOST']) === false)
+			{
+				$replaced_link = "<a href=\"{$href_link}\"{$matches[1]}{$matches[3]} target=\"_blank\">{$matches[4]}</a>";
+			}
+			else
+			{
+				$replaced_link = "<a href=\"{$href_link}\"{$matches[1]}{$matches[3]}>{$matches[4]}</a>";
+			}
+			return $replaced_link;
+		}, $string
+	);
+}, 11, 1 );
